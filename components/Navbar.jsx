e0f0bf068cdfd69aa8f4c2c2019688eb27baaecd
Rahul Yedida <rahul@rahul-pc.rahul-pc>
2017-09-29 23:46:58 +0530

    Minor UI change

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,12 +11,12 @@ class Navbar extends React.Component {
         return (
             <div style={{zIndex: 1000, position: "fixed", width: "100%", height: "64px"}} className="green lighten-1 row">
                 <div className="col s3">
-                    <h3 href="#" style={{bottom: "15px", position: "relative", color: "white"}}>QTube</h3>
+                    <h5 href="#" style={{top: "5px", position: "relative", color: "white"}}><b>QTube</b></h5>
                 </div>
                 <div className="col s6">
-                    <div className="card" style={{position: "absolute", width: "600px", height: "80%"}}>
+                    <div className="card" style={{position: "absolute", width: "600px", height: "70%"}}>
                         <input id="search" placeholder="Search" className="no-material search-bar" />
-                        <i className="material-icons" style={{position: "fixed", marginRight: "10px", paddingTop: "15px"}}>search</i>
+                        <i className="material-icons" style={{position: "fixed", marginRight: "10px", paddingTop: "10px"}}>search</i>
                     </div>
                 </div>
                 <div className="col s3" style={{direction: "rtl"}}>
