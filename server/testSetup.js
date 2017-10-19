b3fa15aaa736aeb38bc4154145065f0862a9f9ed
Rahul Yedida <yrahul3910@gmail.com>
2017-10-19 18:18:53 +0530

    Final revert commit

--- server/testSetup.js
+++
@@ -1,4 +0,0 @@
-// Register Babel to transpile before our tests run
-require("babel-register");
-// Disable Webpack features that Mocha doesn't understand
-require.extensions[".css"] = function() {};
